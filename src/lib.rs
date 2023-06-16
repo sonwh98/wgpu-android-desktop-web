449d487d886d7434d0f2dca622cc58cca086d2a6
Sonny To <son.c.to@gmail.com>
2023-06-15 20:17:37 -0400

    rm comment

--- src/lib.rs
+++ src/lib.rs
@@ -286,10 +286,7 @@ fn _main(event_loop: EventLoop<()>) {
 #[no_mangle]
 fn android_main(app: AndroidApp) {
     use winit::platform::android::EventLoopBuilderExtAndroid;
-
-    //android_logger::init_once(android_logger::Config::default().with_min_level(log::Level::Info));
     android_logger::init_once(android_logger::Config::default().with_max_level(log::LevelFilter::Trace));
-    
     
     let event_loop = EventLoopBuilder::new().with_android_app(app).build();
     _main(event_loop);
